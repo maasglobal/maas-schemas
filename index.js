f8be8647ca6949d443ca7bad0d16cf8e62274426
Hieu Nguyen <blackevil245@gmail.com>
2016-07-22 14:46:38 +0300

    Merge fixes

--- index.js
+++ index.js
@@ -2,9 +2,35 @@
 
 const path = require('path');
 const deref = require('json-schema-deref');
+const Promise = require('bluebird');
+const ajvFactory = require('ajv');
+const ajv = ajvFactory({ verbose: true });
 
-const dereference = schema => (
-  new Promise((resolve, reject) => {
+/**
+ * A simple Promisified schema validator that can resolve nested
+ * refs with absolute path based on a root URL (here project root)
+ *
+ * E.g.
+ * - create refs to schemas as $ref: '/schemas/foo.json'
+ * - call validator
+ * validator.validate(object, schema)
+ *  .then(result => {
+ *    if (result) {
+ *      // We got an error
+ *    }
+ *    else {
+ *      // Result ok, we got null
+ *    }
+ *  }
+ */
+
+/**
+ * Deref json schema
+ * @param {object} schema
+ */
+function derefSchema(schema) {
+  return new Promise((resolve, reject) => {
+
     const options = {
       failOnMissing: true,
       baseFolder: path.resolve('./'),
@@ -17,9 +43,44 @@ const dereference = schema => (
 
       return resolve(dereferenced);
     });
-  })
-);
+
+  });
+}
+
+/**
+ * Get JSON schema located with input path
+ * @param {String} path - path relative to repo
+ */
+function resolveSchemaFromPath(path) {
+  const schema = require(path);
+  return schema;
+}
+
+/**
+ *	Validate an object using schema retrieved from input path
+ *	@param {String} path - path to the requested schema
+ *	@param {Object} object - input testing subject
+ */
+function validate(path, object) {
+  return derefSchema(resolveSchemaFromPath(path))
+    .catch(error => {
+      console.log(error);
+      throw new Error('Runtime error: Invalid JSON schema.');
+    })
+    .then(dereferenced => {
+      // Validate it
+      const validate = ajv.compile(dereferenced);
+      const valid = validate(object);
+
+      if (valid) {
+        return Promise.resolve(null);
+      }
+
+      return Promise.resolve(validate.rrors);
+    });
+}
 
 module.exports = {
-  dereference,
+  validate,
+  derefSchema,
 };
--- test.js
+++ test.js
@@ -5,6 +5,38 @@ const index = require('./index.js');
 const expect = require('chai').expect;
 const glob = require('glob');
 
+// Missing required field [leg, customer]
+const dummyInvalidBooking = {
+  customer: {
+    title: 'mr',
+    firstName: 'John',
+    lastName: 'Doe',
+    phone: 123456,
+    email: 'john.doe@test.fi',
+  },
+};
+
+const dummyValidBooking = {
+  leg: {
+    mode: 'WALK',
+    startTime: Date.now(),
+    endTime: Date.now(),
+  },
+  terms: {
+    price: {
+      amount: 0,
+      currency: 'EUR',
+    },
+  },
+  customer: {
+    title: 'mr',
+    firstName: 'John',
+    lastName: 'Doe',
+    phone: 123456,
+    email: 'john.doe@test.fi',
+  },
+};
+
 const globPromise = (pattern, options) => (
   new Promise((resolve, reject) => {
     glob(pattern, options || {}, (err, matches) => {
@@ -15,27 +47,41 @@ const globPromise = (pattern, options) => (
 );
 
 describe('MaaS.fi schemas', () => {
-  before(done => {
-    return globPromise('*(core|tsp|maas-backend)/**/*.json', { root: path.resolve('./') })
-      .then(schemaPaths => {
-        describe(`Dereferencing tests for ${schemaPaths.length} file(s)`, () => {
-          schemaPaths.forEach(schemaPath => {
-            it(`should be able to dereference ${schemaPath}`, done => {
-              return index.dereference(require(path.resolve(schemaPath)))
-                .then(fullSchema => {
-                  expect(fullSchema).to.not.be.undefined;
-                  done();
-                })
-                .catch(done);
-            });
-          });
-        });
-        done();
-      })
-      .catch(error => done);
-  });
 
-  it('[placeholder to trigger before()]', done => {
-    done();
+  describe('Booking-create', () => {
+    describe('validate valid request', () => {
+
+      let response;
+
+      before(done => {
+        index.validate('./maas-backend/bookings-create/request.json', dummyValidBooking)
+          .then(_response => {
+            console.log(response);
+            response = _response;
+            done();
+          });
+      });
+
+      it('should succeed without error', () => {
+        expect(response).to.be.null;
+      });
+    });
+
+    describe('validate invalid request', () => {
+
+      let response;
+
+      before(done => {
+        index.validate('./maas-backend/bookings-create/request.json', dummyInvalidBooking)
+          .then(_response => {
+            response = _response;
+            done();
+          });
+      });
+
+      it('should have validation error', () => {
+        expect(response).to.not.be.null;
+      });
+    });
   });
 });
