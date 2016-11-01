1bf8a966d4a823d25911c0a64430cdd498f82dac
laurisvan <lauri.svan@iki.fi>
2016-11-01 08:13:35 +0200

    Add multipleOfPrecision option to prevent double precision errors

--- validator.js
+++ validator.js
@@ -63,6 +63,7 @@ function validate(schema, object, options) {
     allErrors: true,
     missingRefs: true,
     extendRefs: true,
+    multipleOfPrecision: 7,
   }, options);
 
   if (typeof ajv === typeof undefined) {
