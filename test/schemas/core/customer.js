c89c63f44f117db581af9095000f1a875c6757b3
Mariusz Nowak <medyk@medikoo.com>
2018-03-07 13:11:17 +0100

    test: generalize generateTestCases util

--- test/schemas/core/customer.js
+++ test/schemas/core/customer.js
@@ -1,29 +1,7 @@
 'use strict';
 
-const expect = require('chai').expect;
 const schema = require('../../../prebuilt/core/components/customer.json');
-const validator = require('../../../validator');
-const ValidationError = require('../../../ValidationError');
-
-function generateTestCases(schema, positive, cases) {
-  if (positive) {
-    it('positive cases', () => {
-      cases.forEach(value => {
-        expect(validator.validateSync(schema, value)).to.be.an.object;
-      });
-    });
-  } else {
-    it('negative cases', () => {
-      cases.forEach(value => {
-        const func = () => {
-          validator.validateSync(schema, value);
-          console.warn(`We should not get here. Input: ${value}`);
-        };
-        expect(func).to.throw(ValidationError);
-      });
-    });
-  }
-}
+const { generateTestCases } = require('../../../test-lib');
 
 describe('customer.firstName', () => {
   generateTestCases(schema.properties.firstName, true, [
