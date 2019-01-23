ac256420c8c62329b763d76b9f737ba1e6e64638
Mariusz Nowak <medyk@medikoo.com>
2019-01-23 15:28:52 +0100

    chore: upgrade eslint config

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,8 @@
 'use strict';
 
 const AJV = require('ajv');
-const eslint = require('gulp-eslint');
 const gulp = require('gulp');
-const jsonclint = require('gulp-json-lint');
-const jsonlint = require('gulp-jsonlint');
 const fg = require('fast-glob');
-
-const jsoncFiles = ['.eslintrc']; // json with comments
-const jsonFiles = ['schemas/**/*.json'];
-const jsFiles = ['*.js', 'test/**/*.js'];
 
 const ajv = new AJV();
 
@@ -24,31 +17,4 @@ gulp.task('ajv-validate', () => {
   schemas.forEach(schema => ajv.compile(require(schema)));
 });
 
-gulp.task('jsonclint', () => {
-  // Unfortunately does not support failOnError at the moment
-  // See https://github.com/panuhorsmalahti/gulp-json-lint/issues/2
-  return gulp
-    .src(jsoncFiles)
-    .pipe(jsonclint({ comments: true }))
-    .pipe(jsonclint.report('verbose'));
-});
-
-gulp.task('jsonlint', () => {
-  return gulp
-    .src(jsonFiles)
-    .pipe(jsonlint())
-    .pipe(jsonlint.reporter())
-    .pipe(jsonlint.failOnError());
-});
-
-gulp.task('eslint', () => {
-  return gulp
-    .src(jsFiles)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-gulp.task('validate', ['jsonclint', 'jsonlint', 'eslint', 'ajv-validate']);
-
-gulp.task('default', ['validate']);
+gulp.task('default', ['ajv-validate']);
