29a9adf04f720b0108327e3bac4cb512a5ae2648
Ruslan Gainutdinov <contact@ruslan.org>
2020-01-17 18:06:11 +0200

    Travis deploy docs (#10) (#11)
    * Travis deploy docs (#10)
    
    * Deploy docs to gh-pages branch when merged to master
    
    * Do deploy docs and npm via two deploy steps
    
    * Travis deploy docs (#12)
    
    * Deploy docs to gh-pages branch when merged to master
    
    * Do deploy docs and npm via two deploy steps
    
    * Fix naming issue

---
+++ maas-schemas/scripts/deply-docs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+yarn build
+yarn docs
+echo Deploying docs to GitHub pages of ${TRAVIS_REPO_SLUG}
+git remote add origin-pages https://${GITHUB_API_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+yarn gh-pages -o origin-pages
