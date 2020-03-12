6755492bec6df7a3607bc96670156e1e9cdf36c6
Michal Czapracki <cz@signicode.com>
2020-03-12 10:05:52 +0000

    Version bump and ts generation

--- maas-schemas-ts/src/maas-backend/booking-option-create/request.ts
+++ maas-schemas-ts/src/maas-backend/booking-option-create/request.ts
@@ -34,9 +34,9 @@ const Defined = t.union([
 export const schemaId =
   'http://maasglobal.com/maas-backend/booking-option-create/request.json';
 
-// Payload
+// Option
 // The purpose of this remains a mystery
-export type Payload = t.Branded<
+export type Option = t.Branded<
   {
     paymentSourceId?: Common_.PaymentSourceId;
     productId?: Product_.Id;
@@ -44,9 +44,9 @@ export type Payload = t.Branded<
     autoPurchaseId?: Units_.Uuid;
     agencyOptions?: AgencyOptions_.AgencyOptions;
   },
-  PayloadBrand
+  OptionBrand
 >;
-export const Payload = t.brand(
+export const Option = t.brand(
   t.partial({
     paymentSourceId: Common_.PaymentSourceId,
     productId: Product_.Id,
@@ -64,12 +64,12 @@ export const Payload = t.brand(
       autoPurchaseId?: Units_.Uuid;
       agencyOptions?: AgencyOptions_.AgencyOptions;
     },
-    PayloadBrand
+    OptionBrand
   > => true,
-  'Payload',
+  'Option',
 );
-export interface PayloadBrand {
-  readonly Payload: unique symbol;
+export interface OptionBrand {
+  readonly Option: unique symbol;
 }
 
 // Request
@@ -77,7 +77,7 @@ export interface PayloadBrand {
 export type Request = t.Branded<
   {
     identityId?: Units_.IdentityId;
-    payload?: Payload;
+    payload?: Array<Option>;
     headers?: ApiCommon_.Headers;
   } & {
     identityId: Defined;
@@ -90,7 +90,7 @@ export const Request = t.brand(
   t.intersection([
     t.partial({
       identityId: Units_.IdentityId,
-      payload: Payload,
+      payload: t.array(Option),
       headers: ApiCommon_.Headers,
     }),
     t.type({
@@ -104,7 +104,7 @@ export const Request = t.brand(
   ): x is t.Branded<
     {
       identityId?: Units_.IdentityId;
-      payload?: Payload;
+      payload?: Array<Option>;
       headers?: ApiCommon_.Headers;
     } & {
       identityId: Defined;
