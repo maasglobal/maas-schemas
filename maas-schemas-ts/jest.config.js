8fa23af27418c866d3d34e74516e326f955d5495
Toni Ruottu <toni.ruottu@maas.global>
2021-11-18 13:25:19 +0200

    Fix jest warning

--- maas-schemas-ts/jest.config.js
+++ maas-schemas-ts/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
       diagnostics: {
         ignoreCodes: [6133, 6196],
       },
