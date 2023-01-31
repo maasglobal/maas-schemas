8afa089c42d3014bf2b1667a88010cb4077f5bb2
Toni Ruottu <toni.ruottu@maas.global>
2023-01-31 14:52:31 +0200

    Run converter

---
+++ maas-schemas/src/ajv/index.ts
@@ -0,0 +1,21 @@
+import * as mjsv from 'maasglobal-json-schema-validator';
+
+let validator: mjsv.Validator | undefined;
+
+export function init(): mjsv.Validator {
+  validator = mjsv.validator([require('./registry')]);
+
+  return validator;
+}
+
+export function cached(): mjsv.Validator {
+  if (typeof validator !== 'undefined') {
+    return validator;
+  }
+  return init();
+}
+
+export function validate(schema: mjsv.Schema | mjsv.SchemaURI, obj: unknown): unknown {
+  const instance = cached();
+  return instance.validate(schema, obj);
+}
